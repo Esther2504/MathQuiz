dbc8edd07c05b9de4176e3d5a82ad6cb4a1d3f50
Esther2504 <esther252000@gmail.com>
2024-04-06 12:15:40 +0200

    setting levels

--- src/App.tsx
+++ src/App.tsx
@@ -8,27 +8,41 @@ const [number3, setNumber3] = useState(1)
 const [operation, setOperation] = useState('+')
 const [guess, setGuess] = useState('')
 const [level, setLevel] = useState<Number>(1)
+const [correctlevel1, setCorrectlevel1] = useState(0)
 
-const operations = ['+', '-', '*']
+const operations = ['+', '-', 'x']
 
 const checkAnswer = (guess: string) => {
   let answer;
   console.log(operation)
   if (operation === '+') {
     answer = number1 + number2
-  } else if (operation == '-') {
+  } else if (operation === '-') {
     answer = number1 - number2
-  } else if (operation == '/') {
+  } else if (operation === '/') {
     answer = number1 / number2
   } else {
     answer = number1 * number2
   }
   
-if (Number(guess) == answer) {
+if (Number(guess) === answer) {
+  if (level === 1) {
+    setNumber1(Math.round(Math.random() * 10))
+    setNumber2(Math.round(Math.random() * 10))
+    setOperation(operations[Math.round(Math.random() * 1)])
+    setCorrectlevel1(correctlevel1 + 1)
+    if (correctlevel1 === 10) {
+      setLevel(2)
+    }
+
+  } else if (level === 2) {
+    setOperation('x')
+  }
   console.log('correct!')
-  setNumber1(Math.round(Math.random() * 20))
-  setNumber2(Math.round(Math.random() * 20))
-  setOperation(operations[Math.round(Math.random() * 3)])
+ 
+  console.log(Math.round(Math.random() * 3))
+  console.log(operation);
+  (document.getElementById('guess') as HTMLInputElement).value = ''
 }
   console.log(answer)
 }
@@ -36,7 +50,8 @@ if (Number(guess) == answer) {
   return (
     <div className="App">
       <header className="App-header">
-        <div>{number1} {operation} {number2} =</div><input type="number" onInput={(e) => checkAnswer((e.target as HTMLInputElement).value)}></input>
+        <p>level: {level.toString()}</p>
+        <div>{number1} {operation} {number2} =</div><input id="guess" type="number" onInput={(e) => checkAnswer((e.target as HTMLInputElement).value)}></input>
       </header>
     </div>
   );
