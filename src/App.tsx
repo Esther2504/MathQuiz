7299cf1fcca289e70c568595a3a04f015f88b477
Esther2504 <esther252000@gmail.com>
2024-04-06 12:41:16 +0200

    seperate functions per operation

--- src/App.tsx
+++ src/App.tsx
@@ -2,56 +2,13 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-const [number1, setNumber1] = useState(1)
-const [number2, setNumber2] = useState(1)
-const [number3, setNumber3] = useState(1)
-const [operation, setOperation] = useState('+')
-const [guess, setGuess] = useState('')
-const [level, setLevel] = useState<Number>(1)
-const [correctlevel1, setCorrectlevel1] = useState(0)
 
-const operations = ['+', '-', 'x']
-
-const checkAnswer = (guess: string) => {
-  let answer;
-  console.log(operation)
-  if (operation === '+') {
-    answer = number1 + number2
-  } else if (operation === '-') {
-    answer = number1 - number2
-  } else if (operation === '/') {
-    answer = number1 / number2
-  } else {
-    answer = number1 * number2
-  }
-  
-if (Number(guess) === answer) {
-  if (level === 1) {
-    setNumber1(Math.round(Math.random() * 10))
-    setNumber2(Math.round(Math.random() * 10))
-    setOperation(operations[Math.round(Math.random() * 1)])
-    setCorrectlevel1(correctlevel1 + 1)
-    if (correctlevel1 === 10) {
-      setLevel(2)
-    }
-
-  } else if (level === 2) {
-    setOperation('x')
-  }
-  console.log('correct!')
- 
-  console.log(Math.round(Math.random() * 3))
-  console.log(operation);
-  (document.getElementById('guess') as HTMLInputElement).value = ''
-}
-  console.log(answer)
-}
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>level: {level.toString()}</p>
-        <div>{number1} {operation} {number2} =</div><input id="guess" type="number" onInput={(e) => checkAnswer((e.target as HTMLInputElement).value)}></input>
+        <p>Points:</p>
+       
       </header>
     </div>
   );
---
+++ src/Plus.tsx
@@ -0,0 +1,59 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function Plus() {
+    const [number1, setNumber1] = useState(1)
+const [number2, setNumber2] = useState(1)
+const [number3, setNumber3] = useState(1)
+const [operation, setOperation] = useState('+')
+const [guess, setGuess] = useState('')
+const [level, setLevel] = useState<Number>(1)
+const [correct, setCorrect] = useState(0)
+
+const operations = ['+', '-', 'x']
+
+const checkAnswer = (guess: string) => {
+  let answer;
+  console.log(operation)
+  if (operation === '+') {
+    answer = number1 + number2
+  } else if (operation === '-') {
+    answer = number1 - number2
+  } else if (operation === '/') {
+    answer = number1 / number2
+  } else {
+    answer = number1 * number2
+  }
+  
+if (Number(guess) === answer) {
+  if (level === 1) {
+    setNumber1(Math.round(Math.random() * 10))
+    setNumber2(Math.round(Math.random() * 10))
+    setOperation(operations[Math.round(Math.random() * 1)])
+    setCorrect(correct + 1)
+    if (correct === 10) {
+      setLevel(2)
+    }
+
+  } else if (level === 2) {
+    setNumber1(Math.round(Math.random() * 50))
+    setNumber2(Math.round(Math.random() * 50))
+    setOperation(operations[Math.round(Math.random() * 1)])
+    setCorrect(correct + 1)
+    if (correct === 20) {
+      setLevel(3)
+    }
+  }
+  console.log('correct!')
+ 
+  console.log(Math.round(Math.random() * 3))
+  console.log(operation);
+  (document.getElementById('guess') as HTMLInputElement).value = ''
+}
+  console.log(answer)
+}
+  return (
+    <div> <p>level: {level.toString()}</p>
+    <div>{number1} {operation} {number2} =</div><input id="guess" type="number" onInput={(e) => checkAnswer((e.target as HTMLInputElement).value)}></input></div>
+  )
+}
