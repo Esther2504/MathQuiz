448a52d72fd55ded44a351779d35c05f36d6fcff
Esther2504 <esther252000@gmail.com>
2024-04-13 14:23:35 +0200

    preventing negative answers

--- src/Minus.tsx
+++ src/Minus.tsx
@@ -16,73 +16,163 @@ export default function Minus() {
     if (Number(guess) === answer) {
       setCorrect(correct + 1)
       if (level === 1) {
-        console.log(number1)
+
+        let newNumber1 = Math.round(Math.random() * 10);
+        let newNumber2 = Math.round(Math.random() * 10);
+
+        while (newNumber1 < newNumber2) {
+          newNumber2 = Math.round(Math.random() * 10);
+        }  
         
-        setNumber1(Math.round(Math.random() * 10))
-          setNumber2(Math.round(Math.random() * (10 - number1 + 1) + number1))
+          setNumber1(newNumber1)
+          setNumber2(newNumber2)
        
         if (correct === 10) {
           setLevel(2)
         }
 
       } else if (level === 2) {
-        setNumber1(Math.round(Math.random() * 50))
-        setNumber2(Math.round(Math.random() * 50))
-        setCorrect(correct + 1)
+
+        let newNumber1 = Math.round(Math.random() * 50);
+        let newNumber2 = Math.round(Math.random() * 50);
+
+        while (newNumber1 < newNumber2) {
+          newNumber2 = Math.round(Math.random() * 50);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+
         if (correct === 20) {
           setLevel(3)
         }
       } else if (level === 3) {
-        setNumber1(Math.round(Math.random() * 100))
-        setNumber2(Math.round(Math.random() * 100))
+
+        let newNumber1 = Math.round(Math.random() * 100);
+        let newNumber2 = Math.round(Math.random() * 100);
+
+        while (newNumber1 < newNumber2) {
+          newNumber2 = Math.round(Math.random() * 100);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+
         if (correct === 30) {
           setLevel(4)
         }
       } else if (level === 4) {
-        setNumber1(Math.round(Math.random() * 500))
-        setNumber2(Math.round(Math.random() * 500))
+
+        let newNumber1 = Math.round(Math.random() * 500);
+        let newNumber2 = Math.round(Math.random() * 500);
+
+        while (newNumber1 < newNumber2) {
+          newNumber2 = Math.round(Math.random() * 500);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+
         if (correct === 40) {
           setLevel(5)
         }
       } else if (level === 5) {
-        setNumber1(Math.round(Math.random() * 1000))
-        setNumber2(Math.round(Math.random() * 1000))
+
+        let newNumber1 = Math.round(Math.random() * 1000);
+        let newNumber2 = Math.round(Math.random() * 1000);
+
+        while (newNumber1 < newNumber2) {
+          newNumber2 = Math.round(Math.random() * 1000);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+
         if (correct === 50) {
           setLevel(6)
         }
       } else if (level === 6) {
-        setNumber1(Math.round(Math.random() * 100))
-        setNumber2(Math.round(Math.random() * 100))
-        setNumber3(Math.round(Math.random() * 100))
+
+        let newNumber1 = Math.round(Math.random() * 100);
+        let newNumber2 = Math.round(Math.random() * 100);
+        let newNumber3 = Math.round(Math.random() * 100);
+
+        while ((newNumber3 - newNumber2 - newNumber1) < 0) {
+          newNumber3 = Math.round(Math.random() * 100);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+        setNumber3(newNumber3)
+
         if (correct === 60) {
           setLevel(7)
         }
       } else if (level === 7) {
-       
-        setNumber1(Math.round(Math.random() * 500))
-        setNumber2(Math.round(Math.random() * 500))
-        setNumber3(Math.round(Math.random() * 500))
+
+        let newNumber1 = Math.round(Math.random() * 500);
+        let newNumber2 = Math.round(Math.random() * 500);
+        let newNumber3 = Math.round(Math.random() * 500);
+
+        while ((newNumber3 - newNumber2 - newNumber1) < 0) {
+          newNumber3 = Math.round(Math.random() * 500);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+        setNumber3(newNumber3)
+
         if (correct === 70) {
           setLevel(8)
         }
       } else if (level === 8) {
-        setNumber1(Math.round(Math.random() * 1000))
-        setNumber2(Math.round(Math.random() * 1000))
-        setNumber3(Math.round(Math.random() * 1000))
+
+        let newNumber1 = Math.round(Math.random() * 1000);
+        let newNumber2 = Math.round(Math.random() * 1000);
+        let newNumber3 = Math.round(Math.random() * 1000);
+
+        while ((newNumber3 - newNumber2 - newNumber1) < 0) {
+          newNumber3 = Math.round(Math.random() * 1000);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+        setNumber3(newNumber3)
+
         if (correct === 80) {
           setLevel(9)
         }
       } else if (level === 9) {
-        setNumber1(Math.round(Math.random() * 5000))
-        setNumber2(Math.round(Math.random() * 5000))
-        setNumber3(Math.round(Math.random() * 5000))
+
+        let newNumber1 = Math.round(Math.random() * 5000);
+        let newNumber2 = Math.round(Math.random() * 5000);
+        let newNumber3 = Math.round(Math.random() * 5000);
+
+        while ((newNumber3 - newNumber2 - newNumber1) < 0) {
+          newNumber3 = Math.round(Math.random() * 5000);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+        setNumber3(newNumber3)
+
         if (correct === 90) {
           setLevel(10)
         }
       } else if (level === 10) {
-        setNumber1(Math.round(Math.random() * 10000))
-        setNumber2(Math.round(Math.random() * 10000))
-        setNumber3(Math.round(Math.random() * 10000))
+
+        let newNumber1 = Math.round(Math.random() * 10000);
+        let newNumber2 = Math.round(Math.random() * 10000);
+        let newNumber3 = Math.round(Math.random() * 10000);
+
+        while ((newNumber3 - newNumber2 - newNumber1) < 0) {
+          newNumber3 = Math.round(Math.random() * 10000);
+        }  
+
+        setNumber1(newNumber1)
+        setNumber2(newNumber2)
+        setNumber3(newNumber3)
+
         if (correct === 100) {
           alert('Congrats')
         }
