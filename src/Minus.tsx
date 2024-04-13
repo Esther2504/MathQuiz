8cc09c7028af9b215aad691a135ea581fefb3f88
Esther2504 <esther252000@gmail.com>
2024-04-13 13:41:31 +0200

    multiply and minus changed levels

--- src/Minus.tsx
+++ src/Minus.tsx
@@ -39,41 +39,43 @@ export default function Minus() {
           setLevel(4)
         }
       } else if (level === 4) {
-        setNumber1(Math.round(Math.random() * 300))
-        setNumber2(Math.round(Math.random() * 300))
+        setNumber1(Math.round(Math.random() * 500))
+        setNumber2(Math.round(Math.random() * 500))
         if (correct === 40) {
           setLevel(5)
         }
       } else if (level === 5) {
-        setNumber1(Math.round(Math.random() * 500))
-        setNumber2(Math.round(Math.random() * 500))
+        setNumber1(Math.round(Math.random() * 1000))
+        setNumber2(Math.round(Math.random() * 1000))
         if (correct === 50) {
           setLevel(6)
         }
       } else if (level === 6) {
-        setNumber1(Math.round(Math.random() * 500))
-        setNumber2(Math.round(Math.random() * 500))
-        setNumber3(Math.round(Math.random() * 500))
+        setNumber1(Math.round(Math.random() * 100))
+        setNumber2(Math.round(Math.random() * 100))
+        setNumber3(Math.round(Math.random() * 100))
         if (correct === 60) {
           setLevel(7)
         }
       } else if (level === 7) {
-        setNumber1(Math.round(Math.random() * 1000))
-        setNumber2(Math.round(Math.random() * 1000))
-        setNumber3(Math.round(Math.random() * 1000))
+       
+        setNumber1(Math.round(Math.random() * 500))
+        setNumber2(Math.round(Math.random() * 500))
+        setNumber3(Math.round(Math.random() * 500))
         if (correct === 70) {
           setLevel(8)
         }
       } else if (level === 8) {
-        setNumber1(Math.round(Math.random() * 5000))
-        setNumber2(Math.round(Math.random() * 5000))
-        setNumber3(Math.round(Math.random() * 5000))
+        setNumber1(Math.round(Math.random() * 1000))
+        setNumber2(Math.round(Math.random() * 1000))
+        setNumber3(Math.round(Math.random() * 1000))
         if (correct === 80) {
           setLevel(9)
         }
       } else if (level === 9) {
-        setNumber1(Math.round(Math.random() * 10000))
-        setNumber2(Math.round(Math.random() * 10000))
+        setNumber1(Math.round(Math.random() * 5000))
+        setNumber2(Math.round(Math.random() * 5000))
+        setNumber3(Math.round(Math.random() * 5000))
         if (correct === 90) {
           setLevel(10)
         }
